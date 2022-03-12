28cacd1ef1f6c2f78ca2b30fefb7b71b10a4d6f2
Work/School <work-school@Spencers-MBP.askey.com>
2022-03-12 13:28:44 -0600

    pushing question 3 git

---
+++ project 1 Greetings/script1.js
@@ -0,0 +1,6 @@
+var name = prompt("Please enter your name");
+var snack = prompt("What is your favorite snack?");
+var drink = prompt("What is your favorite drink?")
+document.write(
+    "Hi there" + name +", it seems that you enjoy eating " + snack + "and drinking" + drink+"!"
+)
